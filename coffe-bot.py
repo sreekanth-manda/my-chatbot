9acbcfed357bcc0cfc640de075635a18266f9b9e
Sreekanth Manda <sreekanth.manda@gmail.com>
2017-05-26 16:47:54 +1000

    Add files via upload

---
+++ coffe-bot.py
@@ -0,0 +1,177 @@
+"""
+This sample demonstrates an implementation of the Lex Code Hook Interface
+in order to serve a sample bot which manages orders for flowers.
+Bot, Intent, and Slot models which are compatible with this sample can be found in the Lex Console
+as part of the 'OrderCoffee' template.
+
+For instructions on how to set up and test this bot, as well as additional samples,
+visit the Lex Getting Started documentation http://docs.aws.amazon.com/lex/latest/dg/getting-started.html.
+"""
+import math
+import datetime
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+
+
+def get_slots(intent_request):
+    return intent_request['currentIntent']['slots']
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+def delegate(session_attributes, slots):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Delegate',
+            'slots': slots
+        }
+    }
+
+
+""" --- Helper Functions --- """
+
+
+def parse_int(n):
+    try:
+        return int(n)
+    except ValueError:
+        return float('nan')
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    if message_content is None:
+        return {
+            "isValid": is_valid,
+            "violatedSlot": violated_slot,
+        }
+
+    return {
+        'isValid': is_valid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 'content': message_content}
+    }
+
+
+def validate_order_coffee(coffee_type, sugar_type, no_of_sugar):
+    coffee_types = ['Mocha', 'Flat-white', 'Latte', 'Cap']
+    if coffee_type is not None and coffee_type not in coffee_types:
+        return build_validation_result(False,
+                                       'CoffeeType',
+                                       'We do not have {}, would you like a different type of Coffee?  '
+                                       'Our most popular coffees are '.format(coffee_type))
+
+    return build_validation_result(True, None, None)
+
+
+""" --- Functions that control the bot's behavior --- """
+
+
+def order_coffee(intent_request):
+    """
+    Performs dialog management and fulfillment for ordering flowers.
+    Beyond fulfillment, the implementation of this intent demonstrates the use of the elicitSlot dialog action
+    in slot validation and re-prompting.
+    """
+
+    coffee_type = get_slots(intent_request)["CoffeeType"]
+    sugar_type = get_slots(intent_request)["SugarType"]
+    no_of_sugar = get_slots(intent_request)["NoOfSugar"]
+    customer_name = get_slots(intent_request)['CustomerName']
+    source = intent_request['invocationSource']
+
+    if source == 'DialogCodeHook':
+        # Perform basic validation on the supplied input slots.
+        # Use the elicitSlot dialog action to re-prompt for the first violation detected.
+        slots = get_slots(intent_request)
+
+        validation_result = validate_order_coffee(coffee_type, sugar_type, no_of_sugar)
+        if not validation_result['isValid']:
+            slots[validation_result['violatedSlot']] = None
+            return elicit_slot(intent_request['sessionAttributes'],
+                               intent_request['currentIntent']['name'],
+                               slots,
+                               validation_result['violatedSlot'],
+                               validation_result['message'])
+
+        # Pass the price of the flowers back through session attributes to be used in various prompts defined
+        # on the bot model.
+        output_session_attributes = intent_request['sessionAttributes']
+        if coffee_type is not None:
+            output_session_attributes['Price'] = 3.5  # Elegant pricing model
+
+        return delegate(output_session_attributes, get_slots(intent_request))
+
+    # Order the flowers, and rely on the goodbye message of the bot to define the message to the end user.
+    # In a real bot, this would likely involve a call to a backend service.
+    return close(intent_request['sessionAttributes'],
+                 'Fulfilled',
+                 {'contentType': 'PlainText',
+                  'content': 'Thanks, your order for {} has been placed and will be ready for pickup'.format(coffee_type)})
+
+
+""" --- Intents --- """
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    print(intent_request)
+
+    #if user_text == "Hi" or user_text == "Hello":
+    #    return ("Hi, I am Sreek")
+
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'TakeMyOrder':
+        return order_coffee(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
